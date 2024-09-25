257844937b5a5d71cab2a334a068000c1f56ff7a
chabuuuu <haphuthinh332004@gmail.com>
2024-09-25 10:30:16 +0700

    build(auto-generate-module): add auto generate module tools

--- main/src/service/account.service.ts
+++ main/src/service/account.service.ts
@@ -2,12 +2,14 @@ import { Account } from '@/models/account.model';
 import { IAccountRepository } from '@/repository/interface/i.account.repository';
 import { BaseCrudService } from '@/service/base/base.service';
 import { IAccountService } from '@/service/interface/i.account.service';
-import { ITYPES } from '@/types/interface.types';
 import { inject, injectable } from 'inversify';
 
 @injectable()
 export class AccountService extends BaseCrudService<Account> implements IAccountService<Account> {
-  constructor(@inject('AccountRepository') repository: IAccountRepository<Account>) {
-    super(repository);
+  private accountRepository: IAccountRepository<Account>;
+
+  constructor(@inject('AccountRepository') accountRepository: IAccountRepository<Account>) {
+    super(accountRepository);
+    this.accountRepository = accountRepository;
   }
 }
--- main/src/service/interface/i.account.service.ts
+++ main/src/service/interface/i.account.service.ts
@@ -1,4 +1,4 @@
 import { IBaseCrudService } from '@/service/interface/i.base.service';
-import { BaseModelType } from '@/types/base-moedl.types';
+import { BaseModelType } from '@/types/base-model.types';
 
 export interface IAccountService<T extends BaseModelType> extends IBaseCrudService<T> {}
