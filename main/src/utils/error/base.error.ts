3855fb254006b02afb420ab685e6aa2b8ff714b5
chabuuuu <haphuthinh332004@gmail.com>
2025-04-07 11:08:15 +0700

    feat(error handle): add http status handle

--- main/src/utils/error/base.error.ts
+++ main/src/utils/error/base.error.ts
@@ -1,7 +1,10 @@
+import { StatusCodes } from 'http-status-codes';
+
 class BaseError extends Error {
   public code: string;
   public msg: string;
   public data?: any;
+  public httpStatus?: StatusCodes;
 
   /**
    * Generate a new instance of BaseException
@@ -9,12 +12,13 @@ class BaseError extends Error {
    * @param msg
    * @param data
    */
-  constructor(code: string, msg: string, data?: any) {
+  constructor(code: string, msg: string, data?: any, httpStatus?: StatusCodes) {
     super(msg);
     Object.setPrototypeOf(this, new.target.prototype);
     this.code = code;
     this.msg = msg;
     this.data = data;
+    this.httpStatus = httpStatus;
     Error.captureStackTrace(this);
   }
 }
